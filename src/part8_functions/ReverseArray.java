1be83b13e2ad62bf38be3bd646ca2c14cf200424
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-25 23:48:29 +0100

    created ReverseArray, Need to finish commit 23:48

---
+++ src/part8_functions/ReverseArray.java
@@ -0,0 +1,20 @@
+package part8_functions;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        //read source data
+        int[] array = {0,1,2,3,4,5,6,7,8,9};
+        //processing
+
+        //display results
+
+    }
+
+    private static void reverse(int[] arr){
+        int len = arr.length;
+        for (int i = 0; i< len; i++){
+            int temp = arr[i];
+
+        }
+    }
+}
