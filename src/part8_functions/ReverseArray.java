ede1b8ea7b545dbd3a937adc3f6bc8ed22ba9942
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-27 13:16:17 +0100

    finished ReverseArray 27.03 commit 13:16

--- src/part8_functions/ReverseArray.java
+++ src/part8_functions/ReverseArray.java
@@ -3,17 +3,23 @@ package part8_functions;
 public class ReverseArray {
     public static void main(String[] args) {
         //read source data
-        int[] array = {0,1,2,3,4,5,6,7,8,9};
+        int[] array = {0,1,2,3,4,5,6,7,8,9,10};
         //processing
-
+        reverse(array);
         //display results
+        for (int num: array) {
+            System.out.print(num + " ");
+        }
 
     }
 
-    private static void reverse(int[] arr){
-        int len = arr.length;
-        for (int i = 0; i< len; i++){
+    private static void reverse(int[] arr) {
+        int len = arr.length ;
+        for (int i = 0; i < len/2; i++) {
             int temp = arr[i];
+            arr[i] = arr[len - 1];
+            arr[len - 1] = temp;
+            len--;
 
         }
     }
