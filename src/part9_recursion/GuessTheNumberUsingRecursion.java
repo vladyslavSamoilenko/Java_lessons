7b528b029eb98b77294cc5da7868aef2a4606e1b
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-28 12:06:32 +0100

    Created GuessTheNumberUsingRecursion commit 12:06

---
+++ src/part9_recursion/GuessTheNumberUsingRecursion.java
@@ -0,0 +1,29 @@
+package part9_recursion;
+
+import part5_cycles.GuessTheNumber;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumberUsingRecursion {
+    public static void main(String[] args) {
+        var number = new Random().nextInt(10);
+        GuessTheNumber( number);
+        }
+
+        private static void GuessTheNumber( int number){
+            System.out.print("Print your number: ");
+            var userCase = new Scanner(System.in).nextInt();
+            if( userCase == number){
+                System.out.println("Congratulations, you guessed the number!");
+            } else if (number < userCase) {
+                System.out.println("number < " + userCase + ". Try again:");
+                GuessTheNumber(number);
+            }else {
+                System.out.println("number > " + userCase + ". Try again:");
+                GuessTheNumber(number);
+            }
+
+        }
+    }
+
