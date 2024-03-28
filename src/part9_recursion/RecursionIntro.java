c48f1bd83cd0871699570ff903ebd7041a4d7545
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-28 11:21:32 +0100

    commit 11:12

--- src/part9_recursion/RecursionIntro.java
+++ src/part9_recursion/RecursionIntro.java
@@ -2,11 +2,11 @@ package part9_recursion;
 
 public class RecursionIntro {
     public static void main(String[] args) {
-        function1(10);
+        function1(100000);
     }
     private static void function1(int count){
         if(count > 0){
-            System.out.print(count + " ");
+            System.out.println(count);
         function1(count - 1);
         }
     }
