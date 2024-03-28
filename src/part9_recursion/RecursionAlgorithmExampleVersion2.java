ebe531bc61ee9b77d8e291bb09978652ca1907a3
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-28 12:54:13 +0100

    commit 12:54

---
+++ src/part9_recursion/RecursionAlgorithmExampleVersion2.java
@@ -0,0 +1,25 @@
+package part9_recursion;
+
+public class RecursionAlgorithmExampleVersion2 {
+    public static void main(String[] args) {
+        printNumbersFrom1To(1);
+        printNumbersFromAndTo(10);
+    }
+
+    private static void printNumbersFrom1To(int i){
+        if(i > 10) {
+            System.out.println();
+        }else {
+            System.out.print(i + "\t");
+            printNumbersFrom1To(++i);
+        }
+    }
+    private static void printNumbersFromAndTo(int i){
+        if(i == 0) {
+            return;
+        }else {
+            printNumbersFromAndTo(i-1);
+            System.out.print(i + "\t");
+        }
+    }
+}
