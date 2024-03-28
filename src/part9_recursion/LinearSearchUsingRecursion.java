61066fba8b84a2a0ec2a51add016d9394db21aee
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-28 16:13:17 +0100

    created LinearSearchUsingRecursion commit 16:13

---
+++ src/part9_recursion/LinearSearchUsingRecursion.java
@@ -0,0 +1,20 @@
+package part9_recursion;
+
+public class LinearSearchUsingRecursion {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10};
+        var query = 10;
+
+        System.out.println(findIndex(array, 0,query));
+    }
+
+    private static int findIndex(int [] array, int i,int query){
+        if (i == array.length) {
+            return -1;
+        }
+        if(array[i] == query){
+            return i;
+        }
+        return findIndex(array, i+1,query);
+    }
+ }
