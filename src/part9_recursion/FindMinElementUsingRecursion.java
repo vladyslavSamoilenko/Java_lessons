26513aa8ca08896ed340e0ed2d077c000f4bb8c1
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-28 15:56:50 +0100

    changed findMin commit 15:56

--- src/part9_recursion/FindMinElementUsingRecursion.java
+++ src/part9_recursion/FindMinElementUsingRecursion.java
@@ -2,8 +2,7 @@ package part9_recursion;
 
 public class FindMinElementUsingRecursion {
     public static void main(String[] args) {
-//        int[] array = {1, -2, 8, 0,-8, 5,-10,-3};
-        int[] array = {1};
+        int[] array = {1, -2, 8, 0,-8, 5,-10,-3};
         int firstElement = array[0];
         System.out.println(findMin(array, 0,firstElement));
     }
