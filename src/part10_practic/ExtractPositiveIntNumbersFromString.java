159c6af687834ff364657d0bbde7db24e42a2750
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-29 23:32:26 +0100

    created ExtractPositiveIntNumbersFromString Need to finish commit 23:32

---
+++ src/part10_practic/ExtractPositiveIntNumbersFromString.java
@@ -0,0 +1,21 @@
+package part10_practic;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ExtractPositiveIntNumbersFromString {
+    public static void main(String[] args) {
+        // read source data
+        String string = "Hello 1234 from 23, or java - is not java11!-2.3 is not a 24";
+        // processing
+        int[] numbers = extractNumbersFromString(string);
+//        // display results
+//        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static int[] extractNumbersFromString(final String string) {
+        for(int i = 0; i < string.length(); i++){
+        }
+        return null;
+    }
+}
