9d6308239780ff74ed5bb58b279b822a4ef20833
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-03-30 22:21:58 +0100

    working on ExtractPositiveIntNumbersFromString Need to finish commit 22:21

--- src/part10_practic/ExtractPositiveIntNumbersFromString.java
+++ src/part10_practic/ExtractPositiveIntNumbersFromString.java
@@ -7,14 +7,27 @@ public class ExtractPositiveIntNumbersFromString {
     public static void main(String[] args) {
         // read source data
         String string = "Hello 1234 from 23, or java - is not java11!-2.3 is not a 24";
+
         // processing
-        int[] numbers = extractNumbersFromString(string);
-      // display results
-       System.out.println(Arrays.toString(numbers));
+        //int[] numbers = extractNumbersFromString(string);
+//      // display results
+//       System.out.println(Arrays.toString(numbers));
     }
 
     private static int[] extractNumbersFromString(final String string) {
-        for(int i = 0; i < string.length(); i++){
+        StringBuilder str = new StringBuilder();
+        for(int i = 0;i < string.length(); i++){
+            char ch = string.charAt(i);
+            if(Character.isDigit(ch)){
+                str.append(ch);
+            }else {
+                if(str.length() > 0){
+                    str.setLength(0);
+                }
+            }
+        }
+        if (str.length() > 0) {
+            System.out.println("Найдено число: " + str);
         }
         return null;
     }
