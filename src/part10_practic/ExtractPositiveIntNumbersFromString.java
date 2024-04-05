d67451dc8c242d0f4cfb536e1ccde65fc4f7f7ff
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-04-05 22:50:43 +0200

    finished ExtractPositiveIntNumbersFromString commit 22:20

--- src/part10_practic/ExtractPositiveIntNumbersFromString.java
+++ src/part10_practic/ExtractPositiveIntNumbersFromString.java
@@ -7,28 +7,39 @@ public class ExtractPositiveIntNumbersFromString {
     public static void main(String[] args) {
         // read source data
         String string = "Hello 1234 from 23, or java - is not java11!-2.3 is not a 24";
-
+        extractNumbersFromString(string);
         // processing
-        //int[] numbers = extractNumbersFromString(string);
-//      // display results
-//       System.out.println(Arrays.toString(numbers));
+        int[] numbers = extractNumbersFromString(string);
+//       display results
+        System.out.println(Arrays.toString(numbers));
     }
 
     private static int[] extractNumbersFromString(final String string) {
-        StringBuilder str = new StringBuilder();
-        for(int i = 0;i < string.length(); i++){
-            char ch = string.charAt(i);
-            if(Character.isDigit(ch)){
-                str.append(ch);
-            }else {
-                if(str.length() > 0){
-                    str.setLength(0);
-                }
+        int[] results = new int[string.length() / 2 + 1];
+        int count = 0;
+        StringBuilder numberBuilder = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            var ch = string.charAt(i);
+            if (Character.isDigit(ch)) {
+                numberBuilder.append(ch);
+            } else if (numberBuilder.length() > 0) {
+                results[count++] = Integer.parseInt(numberBuilder.toString());
+                numberBuilder.setLength(0);
             }
         }
-        if (str.length() > 0) {
-            System.out.println("Найдено число: " + str);
+        if (numberBuilder.length() > 0) {
+            results[count++] = Integer.parseInt(numberBuilder.toString());
+            numberBuilder.setLength(0);
         }
-        return null;
+        return trimArray(results, count);
     }
+    private static int[] trimArray(int[] array, int count){
+        int[] result = new int[count];
+        for (int i = 0; i < count; i++){
+              result[i]=array[i];
+        }
+        return result;
+    }
+
 }
+
