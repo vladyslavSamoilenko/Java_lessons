768cc78b0a62eca74a7719264ade5c5031bb7274
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-04-10 23:09:42 +0200

    done bubblesort commit 23:07

--- src/part10_practic/BubbleSort.java
+++ src/part10_practic/BubbleSort.java
@@ -11,13 +11,18 @@ public class BubbleSort {
 
     private static int [] bubbleSort(int [] array){
         for (int j = 1; j < array.length; j++){
+            boolean swap = false;
             for (int i = 0 ;i < array.length - j ; i++ ){
                if (array[i]>array[i+1]){
                    int temp = array[i];
                    array[i] = array[i+1];
                    array[i + 1] = temp;
+                   swap = true;
                }
         }
+            if (!swap){
+                break;
+            }
         }
         return array;
     }
