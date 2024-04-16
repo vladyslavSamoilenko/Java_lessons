8038f4f1e3d7e695e7ee8aaa877575246eaaa2bf
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-04-17 00:18:39 +0200

    commit 00:18

--- src/part11_decomposition/ReturnNerArray.java
+++ src/part11_decomposition/ReturnNerArray.java
@@ -1,4 +1,26 @@
 package part11_decomposition;
 
+import java.util.Arrays;
+
 public class ReturnNerArray {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getNewArray1()));
+        System.out.println(Arrays.toString(getNewArray2()));
+        System.out.println(Arrays.toString(getNewArray3()));
+    }
+
+    private static int[] getNewArray1() {
+        int [] arr = {1,2,3};
+        return arr;
+    }
+    private static int[] getNewArray2() {
+        int [] arr = new int[3];
+        arr [0] = 1;
+        arr [1] = 2;
+        arr [2] = 3;
+        return arr;
+    }
+    private static int[] getNewArray3() {
+        return new int[]{1,2,3};
+    }
 }
